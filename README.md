4c4c1b5d1f7a4639fbb1d6d3de29b7848b7679fc
Кондрашов Евгений <e.kondrashov@cuberto.ru>
2019-12-26 15:11:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Recruits server
+
+- Run `npm install`
+- Add `.env` file in `server` directory
+- Run `npm start`
---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+if (dotenv.error) {
+  throw dotenv.error;
+}
+
+const { getSecret } = require('./secrets');
+const usersRoute = require('./routes/users');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(getSecret('dbUri')).then(
+  () => {
+    console.log('Connected to mongoDB');
+  },
+  (err) => console.log('Error connecting to mongoDB', err)
+);
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use('/api/users', usersRoute);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = { app };
